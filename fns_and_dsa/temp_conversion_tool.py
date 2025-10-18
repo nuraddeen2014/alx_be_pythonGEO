714c491ec71ea6a7ea9bf4c2ee3fd47c5266346d
Abdulmalik Haruna <93302020+nuraddeen2014@users.noreply.github.com>
2025-10-18 06:36:51 +0100

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,27 +1,22 @@
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    """convert Fahrenheit to Celsius."""
-    return(fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
-
+    return (fahrenheit-32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    """convert to fahrenheit."""
-    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR)+32
+try:
+    enter_temperature = int(input("Enter the temperature to convert: "))
+    celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
 
+    match celsius_or_fahrenheit:
+        case 'C':
+            print(f"{enter_temperature}C is {convert_to_fahrenheit(enter_temperature)}")
 
-def main():
-    temp = input("Enter the temperature to convert: ")
-    question = input("Is this temperaturein celsius or fahrenheit? (C/F): ")
-    if question == "C":
-        converted = convert_to_fahrenheit(float(temp))
-        print(f"{temp}°C is {converted}°F")
-    elif question == "F":
-        converted = convert_to_celsius(float(temp))
-        print(f"{temp}°F is {converted:.2f}°C")
-    else:
-        print("Invalid input. Please enter 'C' for Celsuis or 'F' for fahrenheit")
+        case 'F':
+            print(f"{enter_temperature}F is {convert_to_celsius(enter_temperature)}")
 
-if __name__ == "_main_":
-    main()
+except ValueError:
+    print("Invalid temperature. Please enter a numeric value.") 
+        
