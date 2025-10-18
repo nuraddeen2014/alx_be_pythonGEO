25231693aa0518a7332ffb2656bf529dec8e650a
Abdulmalik Haruna <93302020+nuraddeen2014@users.noreply.github.com>
2025-10-18 07:03:44 +0100

    Update temp_conversion_tool.py more recent

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -8,7 +8,7 @@ def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR)+32
 try:
     enter_temperature = int(input("Enter the temperature to convert: "))
-    celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+    celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/F):").strip().upper()
 
     match celsius_or_fahrenheit:
         case 'C':
